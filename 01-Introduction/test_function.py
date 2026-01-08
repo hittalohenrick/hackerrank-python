09a371c87e648215fd12b26efebf26bebeffeee3
hittalohenrick <henrickhittalo@gmail.com>
2026-01-07 21:49:12 -0400

    feat: add function.py and test_function.py

---
+++ 01-Introduction/test_function.py
@@ -0,0 +1,11 @@
+from function import is_leap 
+
+def test_is_leap():
+    assert is_leap(1800) == False
+    assert is_leap(1900) == False
+    assert is_leap(1990) == False
+    assert is_leap(2000) == True
+    assert is_leap(2100) == False
+    assert is_leap(2200) == False
+    assert is_leap(2300) == False
+    assert is_leap(2500) == False
